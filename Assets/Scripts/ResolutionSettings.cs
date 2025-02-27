ddc992c1d3a7d3252990c8d80dba727f25a075ef
aarngkk <aaronngkk@gmail.com>
2025-02-27 13:28:18 +0800

    Added more low-end resolutions and increased resolution dropdown scroll sensitivity

--- Assets/Scripts/ResolutionSettings.cs
+++ Assets/Scripts/ResolutionSettings.cs
@@ -2,35 +2,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
-using System.Linq; // Add this for distinct resolution filtering
 
 public class ResolutionSettings : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown;
-    private Resolution[] resolutions;
-    private List<Resolution> uniqueResolutions = new List<Resolution>();
+    private List<Resolution> predefinedResolutions = new List<Resolution>()
+    {
+        new Resolution { width = 1920, height = 1080 },
+        new Resolution { width = 1600, height = 900 },
+        new Resolution { width = 1366, height = 768 },
+        new Resolution { width = 1280, height = 720 },
+        new Resolution { width = 1152, height = 648 },
+        new Resolution { width = 1024, height = 576 },
+        new Resolution { width = 960, height = 540 },
+        new Resolution { width = 854, height = 480 },
+        new Resolution { width = 640, height = 360 },
+        new Resolution { width = 1920, height = 1200 },
+        new Resolution { width = 1680, height = 1050 },
+        new Resolution { width = 1440, height = 900 },
+        new Resolution { width = 1280, height = 1024 },
+        new Resolution { width = 1024, height = 768 },
+        new Resolution { width = 2560, height = 1080 },
+        new Resolution { width = 3440, height = 1440 }
+    };
 
     void Start()
     {
-        resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
-
-        // Filter unique width & height resolutions, ignoring refresh rate
-        uniqueResolutions = resolutions
-            .Select(res => new Resolution { width = res.width, height = res.height }) // Remove refresh rate
-            .Distinct()
-            .ToList();
 
         int currentResolutionIndex = 0;
         List<string> options = new List<string>();
 
-        for (int i = 0; i < uniqueResolutions.Count; i++)
+        for (int i = 0; i < predefinedResolutions.Count; i++)
         {
-            string option = uniqueResolutions[i].width + " x " + uniqueResolutions[i].height;
+            string option = predefinedResolutions[i].width + " x " + predefinedResolutions[i].height;
             options.Add(option);
 
-            if (uniqueResolutions[i].width == Screen.currentResolution.width &&
-                uniqueResolutions[i].height == Screen.currentResolution.height)
+            if (predefinedResolutions[i].width == Screen.currentResolution.width &&
+                predefinedResolutions[i].height == Screen.currentResolution.height)
             {
                 currentResolutionIndex = i;
             }
@@ -45,7 +54,7 @@ public class ResolutionSettings : MonoBehaviour
 
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = uniqueResolutions[resolutionIndex];
+        Resolution resolution = predefinedResolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
