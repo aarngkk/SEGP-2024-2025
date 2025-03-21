a12564135e690c885d79485b8d3296295bb20619
FadilahSyed <fadilahsyedazlan@gmail.com>
2025-03-21 21:27:43 +0800

    fonts + camera

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -48,11 +48,11 @@ public class TutorialManager : MonoBehaviour
             tutorialSteps[currentStep].SetActive(true);
 
             // Check if we've just moved onto step #3 (index 2)
-            if (currentStep == 5 && scriptSelectionManager != null)
+            if (currentStep == 6 && scriptSelectionManager != null)
             {
                 scriptSelectionManager.OpenScriptSelection();
             }
-            else if (currentStep == 6 && scriptSelectionManager != null)
+            else if (currentStep == 7 && scriptSelectionManager != null)
             {
                 scriptSelectionManager.CloseScriptSelection();
             }
